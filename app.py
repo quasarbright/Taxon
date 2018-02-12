922878f18cb23ab066dd1a4f8d01bc637e30aea9
quasarbright <mthundershock@gmail.com>
2018-02-11 22:26:06 -0500

    fix capitalization :(

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def relPath(f):
     else:
         # The application is not frozen
         # Change this bit to match where you store your data files:
-        datadir = os.path.dirname(os.path.absPath(__file__))
+        datadir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(datadir, *f)
 if not os.path.isdir(relPath('profiles')):
     os.mkdir(relPath('profiles'))
