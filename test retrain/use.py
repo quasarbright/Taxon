bf006d9feed588393a00e29a6f859c5df9b5205f
quasarbright <mthundershock@gmail.com>
2018-02-17 23:46:31 -0500

    update use

--- test retrain/use.py
+++ test retrain/use.py
@@ -1,4 +1,6 @@
-import retrain
+import retrain, label_image
 myargs = ['--image_dir', 'D:/code/flower_neural_network/flower_photos', '--output_graph', 'D:\\code\\Github\\codes-at-home\\profiles\\asdf\\output_graph.pb', '--intermediate_output_graphs_dir', 'D:\\code\\Github\\codes-at-home\\profiles\\asdf\\intermediate_out', '--output_labels', 'D:\\code\\Github\\codes-at-home\\profiles\\asdf\\output_labels.txt', '--summaries_dir',
           'D:\\code\\Github\\codes-at-home\\profiles\\asdf\\summaries', '--bottleneck_dir', 'D:\\code\\Github\\codes-at-home\\profiles\\asdf\\bottleneck_dir', '--how_many_training_steps', '1000', '--model_dir', 'D:\\code\\Github\\codes-at-home\\profiles\\asdf\\model_dir']
-retrain.myFunc(myargs)
+# retrain.myFunc(myargs)
+myargs = ['--graph', 'D:\\code\\Github\\codes-at-home\\profiles\\asdf\\output_graph.pb', '--labels=D:\\code\\Github\\codes-at-home\\profiles\\asdf\\output_labels.txt', '--input_layer=Mul', '--output_layer=final_result', '--input_mean=128', '--input_std=128', '--image=D:/code/flower_neural_network/flower_photos/sunflowers/40410814_fba3837226_n.jpg']
+label_image.myFunc(myargs)
